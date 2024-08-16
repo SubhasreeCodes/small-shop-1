fbf1bc487b344ba30400a6a0b8e7b0f69ef252ac
SubhasreeCodes <subhasreeps2001@gmail.com>
2024-08-16 11:29:25 +0530

    Update CountrySeeder.php

--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -5,12 +5,15 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Illuminate\Support\Facades\DB;
+use App\Models\Country;
+
 class CountrySeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
         // You can add more countries as needed
         $countries = [
