fd06b08ee65edb00c77bff6eb993575104a973a2
SubhasreeCodes <subhasreeps2001@gmail.com>
2024-08-16 11:34:43 +0530

    Update CitySeeder.php

--- database/seeders/CitySeeder.php
+++ database/seeders/CitySeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\State;
+use App\Models\City;
+
 class CitySeeder extends Seeder
 {
     /**
