8165f7885a52a65157373646306b20aa1b394ea2
SubhasreeCodes <subhasreeps2001@gmail.com>
2024-09-14 12:48:31 +0530

    Update 2024_09_14_065803_create_brands_table.php
    Brand's migration table

--- database/migrations/2024_09_14_065803_create_brands_table.php
+++ database/migrations/2024_09_14_065803_create_brands_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('image_path')->default("no_image_available.png");
             $table->timestamps();
         });
     }
