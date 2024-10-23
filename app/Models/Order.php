ddb7ffa744bd8991e379cab16cac14e6584cb530
SubhasreeCodes <subhasreeps2001@gmail.com>
2024-10-23 19:34:03 +0530

    Update Order.php
    To relate the entry at dashboard

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -11,11 +11,76 @@ class Order extends Model
 
     protected $fillable = [
 
-        'customer_id',
         'order_number',
+        'customer_id',
         'order_date',
         'total_amount',
         'order_status', 
         'payment_method'
     ];
+
+    
+
+    public function customer()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orderItems(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class, 'order_items');
+    }
+
+    public function calculaterSGST(): float
+    {
+        return $this->total_amount * 0.06; // Corrected the calculation
+    }
+
+    public function calculaterCGST(): float
+    {
+        return $this->total_amount * 0.06; // Corrected the calculation
+    }
+
+    public function calculate_SGST_CGST(): float 
+    {
+        return $this->calculaterSGST() + $this->calculaterCGST();
+    }
+
+    public function total_amount_with_SGST_CGST(): float
+    {
+        // $total_amount = $this->total_amout;
+
+        // $total_amount += $this->calculaterSGST();
+
+        // $total_amount += $this->calculaterCGST();
+
+        // return $total_amount;
+
+        return $this->total_amount + $this->calculaterSGST() + $this->calculaterCGST();
+    }
+
+    public function getTotalAmountInWords(): string
+    {
+        $numberToWords = new NumberToWords();
+        $transformer = $numberToWords->getNumberTransformer('en');
+        return $transformer->toWords($this->total_amount);
+    }
+
+    public function convertTaxAmountInWords(): string
+    {
+        $numberToWords = new NumberToWords();
+        $transformer = $numberToWords->getNumberTransformer('en');
+        $taxAmount = $this->calculaterSGST() + $this->calculaterCGST(); // Calculated tax amount
+        return $transformer->toWords($taxAmount);
+    }
+
+    public function history()
+    {
+        return $this->hasMany(OrderHistory::class, 'order_id');
+    }
 }
